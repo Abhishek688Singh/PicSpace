e5e5de05dbb3891ca91b8d248704506f5dd83139
Abhishek Singh <singh752007abhishek@gmail.com>
2025-08-02 21:44:31 +0530

    custom auth not work v1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,2 @@
-// import { handlers } from "@/auth" // Referring to the auth.ts we just created
-// export const { GET, POST } = handlers
-
-
-// /app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
-import { authOptions } from "./authOptions";
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+import { handlers } from "@/auth" // Referring to the auth.ts we just created
+export const { GET, POST } = handlers
--- auth.ts
+++ auth.ts
@@ -1,13 +1,97 @@
-// auth.ts
-import NextAuth from "next-auth";
-import { authOptions as baseAuthOptions } from "./app/api/auth/[...nextauth]/authOptions";
+import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { pool } from "./lib/db";
 
-import type { NextAuthConfig } from "next-auth";
-// import { authOptions } from ;
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [Google,
 
-export const authOptions: NextAuthConfig = {
-  ...baseAuthOptions,
-  trustHost: true,
-};
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const email = credentials?.email?.toString() || "";
+        const password = credentials?.password?.toString() || "";
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
+        if (!email || !password) {
+          throw new Error("Missing credentials");
+        }
+
+        const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        const user = result.rows[0];
+        
+        if (!user) {
+          throw new Error("No user found with that email");
+        }
+
+         if (user.password_hash === "google") {
+          throw new Error("This account uses Google Sign-In. Please use Google to log in.");
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password_hash);
+        if (!passwordMatch) {
+          throw new Error("Invalid password");
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
+      }
+      ,
+    })
+  ],
+
+  pages: {
+    signIn: "/login", // ⬅️ optional, tells NextAuth where your custom login page is
+  },
+
+  session: {
+    strategy: 'jwt',   // or 'database'
+    maxAge: 60 * 60 * 60,
+    // updateAge: 0, // how often the session is refreshed
+  },
+  callbacks: {
+    async signIn({ user }) {
+      const existingUser = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [user.email]
+      );
+
+      if (existingUser.rows.length === 0) {
+        await pool.query(
+          "INSERT INTO users (name, email, password_hash, user_image) VALUES ($1, $2, $3, $4)",
+          [user.name, user.email, 'Google', user.image]
+        );
+      }
+
+      return true;
+    },
+
+    async jwt({ token }) {
+      const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+        token.email,
+      ]);
+      if (result.rows.length > 0) {
+        token.id = result.rows[0].id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+
+
+
+})
