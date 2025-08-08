7998e01023b78b3ab6943ecef1750b3c4dce9944
Abhishek Singh <singh752007abhishek@gmail.com>
2025-08-08 22:11:57 +0530

    dashboard->image->page.tsx fix

--- app/dashbord/[workspace-id]/[details]/images/page.tsx
+++ app/dashbord/[workspace-id]/[details]/images/page.tsx
@@ -1,57 +1,54 @@
-import React from 'react'
-
+import React from 'react';
 import Images from './ImageUpload';
 import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import ImageGallery from '@/components/ImageGallery';
 import { pool } from '@/lib/db';
 
-const page = async ({ params }: {
-  params: { 'workspace-id': string; details: string };
-}) => {
-  const session = await auth();
-  if (!session) redirect("/login");
+type PageProps = {
+  params: {
+    'workspace-id': string;
+    details: string;
+  };
+};
 
-  const { 'workspace-id': workspaceId, details: memberFriendId } = params;
+export default async function Page({ params }: PageProps) {
+  const workspaceId = params['workspace-id'];
+  const memberFriendId = params.details;
+
+  const session = await auth();
+  if (!session) redirect('/login');
 
   const result = await pool.query(
-    "SELECT created_at ,content ,public_id FROM folder_items WHERE workspace_id = $1 AND shared_with = $2 AND type = 'image'",
+    `SELECT created_at, content, public_id 
+     FROM folder_items 
+     WHERE workspace_id = $1 
+       AND shared_with = $2 
+       AND type = 'image'`,
     [workspaceId, memberFriendId]
   );
+
   const publicIds = result.rows;
-  // console.log(publicIds)
 
   return (
-    <div className='flex flex-col items-center
-    pt-[150]  text-white h-[100vh]'>
-
+    <div className="flex flex-col items-center pt-[150] text-white h-[100vh]">
       {/* image uploading component */}
       <Images workspaceId={workspaceId} memberFriendId={memberFriendId} />
-      
-<hr className='w-[90%] border-[1px] bg-amber-50' />
 
-
+      <hr className="w-[90%] border-[1px] bg-amber-50" />
 
       {/* IMAGE VIEWING COMPONENT */}
       <h1 className="text-2xl font-bold text-center mt-6">Shared by Me :</h1>
-      
-      { publicIds.length === 0  ? (
-        <div className='flex flex-col items-center pt-[100] 
-        text-bold text-teal-100  text-4xl'>
+
+      {publicIds.length === 0 ? (
+        <div className="flex flex-col items-center pt-[100] text-bold text-teal-100 text-4xl">
           <h1>NO Image Shared</h1>
         </div>
       ) : (
         <div>
-          
           <ImageGallery images={publicIds} />
         </div>
       )}
-      
-      
-
-
     </div>
-  )
+  );
 }
-
-export default page
